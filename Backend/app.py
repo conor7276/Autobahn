c75b1be8d002337bdea42ed82635bc9797654187
conor7276 <conor7276@gmail.com>
2023-10-25 17:01:06 -0400

    tested pushing data to frontend

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
 
+from dotenv import load_dotenv
+import psycopg2
+
+import os
+from os.path import join, dirname
+import json
+
+
+dotenv_path = join(dirname(__file__),'.env')
+
+load_dotenv(dotenv_path)
+
+DB_NAME = os.environ.get("DB_NAME")
+DB_HOST = os.environ.get("DB_HOST")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_PORT = os.environ.get("DB_PORT")
 
 app = Flask(__name__)
 
@@ -12,11 +29,42 @@ def home():
     print("Server Up")
     return "Server up"
 
-@app.route("/hello") # localhost:5000/hello
+# @app.route("/hello") # localhost:5000/hello
+# def hello_world():
+#     print("Hello World")
+#     greeting = {"Hello" : "World"}
+#     return greeting
+
+@app.route("/hello")
 def hello_world():
-    print("Hello World")
-    greeting = {"Hello" : "World"}
-    return greeting
+    connection = psycopg2.connect(database = DB_NAME,
+                            host = DB_HOST,
+                            user = DB_USER,
+                                password = DB_PASSWORD,
+                                port = DB_PORT )
+
+
+    # connect to database with cursor to access data
+    curr = connection.cursor()
+
+    print("Attempting the great SQL Creation")
+    try:
+        # execute sql statements
+        curr.execute("SELECT * FROM customer;")
+
+        data = curr.fetchall()
+        print(type(data))
+        print(data)
+        return data
+    except:
+        print("Machine broke gg")
+
+    print("WE FUCKING DID IT")
+
+
+    connection.commit() # save changes made
+    connection.close() # close the connection pls
+    curr.close() # close the cursor as well
 
 if __name__ == '__main__':
     app.run()
