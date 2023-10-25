c6924d41d37a296ce17ded4b4f9d6b6bfda3a2b5
Aleksandros Doci <aleksandrosdoci1@gmail.com>
2023-10-25 11:29:58 -0400

    test

--- Backend/app.py
+++ Backend/app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/hello")
+
 def hello_world():
     print("Hello World")
     return {"Greetings" : "Hello, World"}
+
+@app.route("/bye")
+def bye_world():
+    print("Bye World")
+    return {"Greetings" : "Bye, World"}
 
 if __name__ == '__main__':
     app.run()
