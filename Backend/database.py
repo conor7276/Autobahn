14deb500c280a3d3976f1dddecce63e79ed5e49e
conor7276 <conor7276@gmail.com>
2023-10-28 14:38:55 -0400

    cleaned up codebase

--- Backend/database.py
+++ Backend/database.py
@@ -30,7 +30,6 @@ connection = psycopg2.connect(database = DB_NAME,
 # connect to database with cursor to access data
 curr = connection.cursor()
 
-print("Attempting the great SQL Creation")
 try:
     # execute sql statements
     curr.execute("INSERT INTO cars (price,photos,issold,description,engine,country,year,name,brand,bodytype) VALUES('100000','https://media4.speedcafe.com/wp-content/uploads/2021/01/Porsche-911-Turbo-S-003-scaled.jpg','false','NICE CAR','Strong','Germany','2021','911 Turbo S Coupe','Porsche','Coupe');")
@@ -40,21 +39,18 @@ try:
     print(data)
 
 except(Exception, psycopg2.Error) as error:
-    print("Machine broke gg ", error)
-
-print("WE FUCKING DID IT")
-
+    print(error)
 
 connection.commit() # save changes made
 connection.close() # close the connection pls
 curr.close() # close the cursor as well
 
 # Create customer table
-# CREATE TABLE IF NOT EXISTS Customer (customerid serial PRIMARY KEY, name varchar, email varchar, password varchar, phonenumber varchar);
+# curr.execute("CREATE TABLE IF NOT EXISTS Customer (customerid serial PRIMARY KEY, name varchar, email varchar, password varchar, phonenumber varchar);")
 # Insert customer
-# INSERT INTO customer (name, email, password, phonenumber) VALUES('Conor','conor@test.com','pass','7181112222');
+# curr.execute("INSERT INTO customer (name, email, password, phonenumber) VALUES('Conor','conor@test.com','pass','7181112222');")
 # Create cars table
-# CREATE TABLE cars (carid serial PRIMARY KEY, price decimal(10,2), photos text, issold boolean, description text, engine varchar, country varchar, year int, name varchar, brand varchar, bodytype varchar);
+# curr.execute("CREATE TABLE cars (carid serial PRIMARY KEY, price decimal(10,2), photos text, issold boolean, description text, engine varchar, country varchar, year int, name varchar, brand varchar, bodytype varchar);")
 ## curr.execute("INSERT INTO cars (price,photos,issold,description,engine,country,year,name,brand,bodytype) VALUES('100000','https://media4.speedcafe.com/wp-content/uploads/2021/01/Porsche-911-Turbo-S-003-scaled.jpg','false','NICE CAR','Strong','Germany','2021','911 Turbo S Coupe','Porsche','Coupe');")
 #  curr.execute("INSERT INTO cars (price,photos,issold,description,engine,country,year,name,brand,bodytype) VALUES('80000','https://cdn.carsbite.com/articles/59276_IMG-20210511-WA0016.jpg','false','NICE CAR2','Strong2','Germany','2023','Mecan Electric','Porsche','SUV');")
 # curr.execute("INSERT INTO cars (price,photos,issold,description,engine,country,year,name,brand,bodytype) VALUES('120000','https://www.carscoops.com/wp-content/uploads/2019/08/3969cea6-audi-rs7-rendering.jpg','false','NICE CAR3','Strong3','Germany','2023','RS7 Sportback','Audi','Sedan');")
